f949905e723cfeceeabe8446917b6f9905bfb49a
Meikandan <meikandai@syncfusion.com>
2019-10-23 10:29:36 +0530

    Updated a simple demo for compiled binding

---
+++ CompiledBinding/CompiledBinding/CompiledBinding/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompiledBinding.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ CompiledBinding/CompiledBinding/CompiledBinding/ViewModels/UserViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CompiledBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CompiledBinding.ViewModels
+{
+    public class UserViewModel
+    {
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+
+        public ObservableCollection<User> Users
+        {
+            get
+            {                
+                return users;
+            }
+            set
+            {
+                users = value;
+            }
+        }
+
+        public UserViewModel()
+        {
+            users = new ObservableCollection<User>()
+            {
+                new User
+                {
+                    Name = "Meikandan",
+                    Age = 32
+                },
+                new User
+                {
+                    Name = "Aathirai",
+                    Age = 4
+                }
+            };            
+        }
+    }
+}
