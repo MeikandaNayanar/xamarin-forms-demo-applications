d3809440784b26b08f57edb621cb57c489c7ccac
Meikanda Nayanar I <meikandai@syncfusion.com>
2019-12-17 17:52:36 +0530

    Updated the Map with Lottie animation sample

---
+++ MapsWithLottie/MapsWithLottie/MapsWithLottie/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapsWithLottie
+{
+    public class Model
+    {
+        public Model(string country, string con)
+        {
+            this.Country = country;
+            this.Continent = con;
+        }
+        public string Continent
+        {
+            get;
+            set;
+        }
+        public string Country
+        {
+            get;
+            set;
+        }
+
+    }
+}
